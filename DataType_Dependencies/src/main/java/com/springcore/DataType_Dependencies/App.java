a7357a765a802d7d3649355fcab5f168abd51440
PremPinreddy <93468791+PremPinreddy@users.noreply.github.com>
2025-04-29 23:40:39 +0530

    Update App.java
    sysout change to check pull from eclipse

--- DataType_Dependencies/src/main/java/com/springcore/DataType_Dependencies/App.java
+++ DataType_Dependencies/src/main/java/com/springcore/DataType_Dependencies/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Hi Prem Chandra Reddy" );
         Student std=new Student(25, "Prem Chandra Reddy", "Hyderabad");
         Student std1= new Student();
         std1.setstudentId(046);
